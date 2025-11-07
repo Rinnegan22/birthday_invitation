248efced267adad7d222f5db9232d7416999e30a
jenrey galanida <jenreygalanida22@gmail.com>
2025-11-08 00:51:37 +0800

    initial commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <span>Hello World</span>
+    <div class="text-center">
+        <v-btn color="primary" append-icon="mdi-account-circle" prepend-icon="mdi-check-circle">
+            <template v-slot:prepend>
+                <v-icon color="success"></v-icon>
+            </template>
+
+            Button
+
+            <template v-slot:append>
+                <v-icon color="warning"></v-icon>
+            </template>
+        </v-btn>
+    </div>
+</template>
+<script setup></script>
